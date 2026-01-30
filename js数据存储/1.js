93f0eaef535a63618cd35e058cba8008efefd988
dorothy900 <mashblockii@163.com>
2026-01-30 23:06:46 +0000

    1/30

---
+++ js数据存储/1.js
@@ -0,0 +1,14 @@
+let str = '[a,b,c]'  //String
+
+let num = 123 //Number
+console.log(num)
+console.log(num.toString())
+
+let flag = true //Boolean
+
+let sy1 = Symbol('hello')
+let sy2 = Symbol('hello')
+console.log(sy1 === sy2)
+
+900000000000000000n+1n
+
---
+++ js数据存储/2.js
@@ -0,0 +1,19 @@
+let obj = {
+    name : 'melena',
+    age : 18,
+    health: 100,
+    flame:function(){
+        console.log('flame')
+    }
+
+}
+
+
+function foo(){
+    var a = '梅琳娜'
+    var b = a
+    var c = {name:'梅瑟莫'}
+    var d = c
+
+}
+foo()
